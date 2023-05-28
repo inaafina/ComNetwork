cba03857685924fad2db0ffac02759779b30e53b
inaafina <80028622+inaafina@users.noreply.github.com>
2023-05-28 15:09:17 +0700

    Update client.py

--- client.py
+++ client.py
@@ -1,27 +1,47 @@
-from socket import *
+import socket
+import webbrowser
 
-serverName = input('IP Address : ')
-serverPort = int(input('Port : '))
+# Server configuration
+SERVER_HOST = '127.0.0.1'  # Server IP address
+SERVER_PORT = 12345  # Server port number
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverName,serverPort))
+# File to request from the server
+file_name = input('Enter the file name: ')
 
-filename = input('The file you want to retrieve: ')
+# Create a TCP socket
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-request = f"GET /{filename} HTTP/1.1\r\nHost: {serverName}\r\n\r\n"
-clientSocket.send(request.encode())
+try:
+    # Connect to the server
+    client_socket.connect((SERVER_HOST, SERVER_PORT))
 
-response = clientSocket.recv(1024)
+    # Send the file name to the server
+    client_socket.sendall(file_name.encode())
 
-print(response.decodce()) # Print the HTTP response message
+    # Receive the file data from the server
+    file_data = client_socket.recv(1024)
 
-if response.decode().startswith('HTTP/1.1 200 OK'): # Save the content of the requested file to a local file
-    with open(filename, 'wb') as f:
-        while True:
-            data = clientSocket.recv(4096)
-            if not data:
-                break
-            f.write(data)
-    print('File saved to ',filename)
+    if file_data == b'File not found':
+        print('404 File not found on the server')
+        # Display a 404 Not Found message in the web browser
+        html_content = '<h1>404 Not Found</h1>'
+        with open('404.html', 'w') as html_file:
+            html_file.write(html_content)
+        webbrowser.open('404.html')
 
-clientSocket.close()
+    else:
+        # Save the file
+        with open(file_name, 'wb') as file:
+            file.write(file_data)
+        print('200 OK')
+        print(format(file_name),'received from the server')
+
+        # Open the file in the web browser
+        webbrowser.open(file_name)
+
+except ConnectionRefusedError:
+    print('Unable to connect to the server')
+
+finally:
+    # Close the socket
+    client_socket.close()
