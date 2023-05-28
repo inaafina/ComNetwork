5fe1cbfdf8c4cd81f42122eccbc70583ab2a8e01
inaafina <80028622+inaafina@users.noreply.github.com>
2023-05-28 15:07:18 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,54 +1,49 @@
 import socket
 import os
 
+# Server configuration
+HOST = '127.0.0.1'  # Server IP address
+PORT = 12345  # Server port number
+FILE_DIRECTORY = './files/'  # Directory to store files
+
+# Create the directory if it doesn't exist
+if not os.path.exists(FILE_DIRECTORY):
+    os.makedirs(FILE_DIRECTORY)
+    
 # create a TCP socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # bind the socket to a specific address and port
-server_address = ('localhost', 8080)
+server_address = (HOST, PORT)
 server_socket.bind(server_address)
 
 # listen for incoming connections
 server_socket.listen(1)
 
-print(f"Server is on {server_address}")
+print('Server listening on {}:{}'.format(HOST, PORT))
 
 while True:
-    # wait for a client connection
+    # Accept client connections
     client_socket, client_address = server_socket.accept()
-    
-    # receive the HTTP request
-    request = client_socket.recv(1024).decode('utf-8')
-    print(f"Received request from {client_address}:")
-    print(request)
-    
-    # parse the request to get the requested file path
-    file_path = request.split()[1]
-    print(f"Requested file path: {file_path}")
-    
-    # get the absolute path of the requested file
-    absolute_path = os.path.abspath(file_path.strip('/'))
+    print('Client connected:', client_address)
 
-    # check if the file exists
-    if not os.path.exists(absolute_path):
-        # if the file does not exist, send a 404 Not Found response
-        response_status = "HTTP/1.1 404 Not Found\r\n"
-        response_body = "<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1></body></html>"
-    else:
-        # if the file exists, read its contents
-        with open(absolute_path, 'r') as f:
-            file_contents = f.read()
-        
-        # create the HTTP response message
-        response_status = "HTTP/1.1 200 OK\r\n"
-        response_headers = f"Content-Type: text/html\r\nContent-Length: {len(file_contents)}\r\n"
-        response_body = file_contents
-    
-    # combine the response components into a single message
-    response_message = response_status + response_headers + "\r\n" + response_body
-    
-    # send the response to the client
-    client_socket.sendall(response_message.encode('utf-8'))
+    # Receive the file name from the client
+    file_name = client_socket.recv(1024).decode()
+    file_path = os.path.join(FILE_DIRECTORY, file_name)
 
-    # close the client connection
+    try:
+        # Open the requested file
+        with open(file_path, 'rb') as file:
+            # Read the file contents
+            file_data = file.read()
+
+        # Send the file data to the client
+        client_socket.sendall(file_data)
+        print('File "{}" sent to {}'.format(file_name, client_address))
+
+    except FileNotFoundError:
+        # File not found
+        client_socket.sendall(b'File not found')
+
+    # Close the client connection
     client_socket.close()
